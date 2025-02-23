afa1712f6627c166bb461ea153d2a4d5eecfcbff
kikem <kikem@DESKTOP-NFD6NUR>
2025-02-23 18:27:49 +0100

    Actualización base de datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,22 +2,22 @@
 
 --Para giis.demo.tkrun:
 insert into Autor(id,nombre,organizacion,grupo,revisor) values 
-	(1, 'Ana García', 'Universidad de Madrid', 'IA y ML', TRUE),
-	(2, 'Luis Fernández', 'TechCorp', 'Desarrollo Web', FALSE),
-	(3, 'María López', 'InnovateX', 'Ciencia de Datos', TRUE),
-	(4, 'Carlos Sánchez', 'Data Solutions', 'Big Data', FALSE),
-	(5, 'Laura Martín', 'StartUp Inc.', 'Seguridad Informática', TRUE);
+    (1, 'Ana García', 'Universidad de Madrid', 'IA y ML', TRUE),
+    (2, 'Luis Fernández', 'TechCorp', 'Desarrollo Web', FALSE),
+    (3, 'María López', 'InnovateX', 'Ciencia de Datos', TRUE),
+    (4, 'Carlos Sánchez', 'Data Solutions', 'Big Data', FALSE),
+    (5, 'Laura Martín', 'StartUp Inc.', 'Seguridad Informática', TRUE);
 
 insert into Autor_articulo(idAutor,idArticulo) values 
-	(1,2),
-	(2,2),
-	(3,3),
-	(4,4),
-	(5,5);
+    (1,2),
+    (2,2),
+    (3,3),
+    (4,4),
+    (5,5);
 
-insert into Articulo(id,titulo,palabras_clave,resumen,fichero,fecha) values 
-	(1, 'Inteligencia Artificial en la Educación', 'IA, educación, aprendizaje','Este artículo explora el impacto de la inteligencia artificial en el aprendizaje y la educación moderna.', 'ia_educacion.pdf', '2024-01-15'),
-	(2, 'Desarrollo Web con React', 'React, desarrollo web, JavaScript','Guía práctica para construir aplicaciones web dinámicas utilizando React y JavaScript.', 'react_web.pdf', '2024-02-20'),
-	(3, 'Análisis de Datos en Marketing', 'datos, marketing, análisis','Estrategias de análisis de datos para optimizar campañas de marketing y mejorar la segmentación de audiencias.', 'marketing_datos.pdf', '2024-03-10'),
-	(4, 'Big Data y su Impacto en la Economía', 'big data, economía, análisis','Estudio sobre cómo el Big Data está transformando la economía global y la toma de decisiones empresariales.', 'big_data_economia.pdf', '2024-04-05'),
-	(5, 'Ciberseguridad en la Era Digital', 'ciberseguridad, privacidad, digital','Análisis de los desafíos de ciberseguridad en el mundo digital y estrategias para proteger la información.', 'ciberseguridad.pdf', '2024-05-12');
+insert into Articulo(id,titulo,palabras_clave,resumen,fichero,fecha,revisado_por) values 
+    (1, 'Inteligencia Artificial en la Educación', 'IA, educación, aprendizaje','Este artículo explora el impacto de la inteligencia artificial en el aprendizaje y la educación moderna.', 'ia_educacion.pdf', '2024-01-15',NULL),
+    (2, 'Desarrollo Web con React', 'React, desarrollo web, JavaScript','Guía práctica para construir aplicaciones web dinámicas utilizando React y JavaScript.', 'react_web.pdf', '2024-02-20',NULL),
+    (3, 'Análisis de Datos en Marketing', 'datos, marketing, análisis','Estrategias de análisis de datos para optimizar campañas de marketing y mejorar la segmentación de audiencias.', 'marketing_datos.pdf', '2024-03-10',NULL),
+    (4, 'Big Data y su Impacto en la Economía', 'big data, economía, análisis','Estudio sobre cómo el Big Data está transformando la economía global y la toma de decisiones empresariales.', 'big_data_economia.pdf', '2024-04-05',NULL),
+    (5, 'Ciberseguridad en la Era Digital', 'ciberseguridad, privacidad, digital','Análisis de los desafíos de ciberseguridad en el mundo digital y estrategias para proteger la información.', 'ciberseguridad.pdf', '2024-05-12',NULL);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,16 +2,17 @@
 --(en este caso en cada aplicacion se usa solo una tabla, por lo que no hace falta)
 
 --Para giis.demo.tkrun:
-drop if exists table Autor_articulo;
-drop if exists table Autor;
-drop if exists table Articulo;
+
+DROP TABLE IF EXISTS Autor_articulo;
+DROP TABLE IF EXISTS Articulo;
+DROP TABLE IF EXISTS Autor;
 
 create table Autor(id int primary key not null, nombre varchar not null, organizacion varchar not null, grupo varchar not null, revisor boolean not null);
 
 
 create table Autor_articulo(idAutor int not null, idArticulo int not null, PRIMARY KEY(idAutor,idArticulo),
-	FOREIGN KEY(idAutor) REFERENCES Autor(id), FOREIGN KEY(idArticulo) REFERENCES Articulo(id));
+    FOREIGN KEY(idAutor) REFERENCES Autor(id), FOREIGN KEY(idArticulo) REFERENCES Articulo(id));
 
 
 create table Articulo(id int primary key not null, titulo varchar not null, palabras_clave varchar not null, resumen varchar not null,
- fichero varchar not null, fecha date not null);
+ fichero varchar not null, fecha date not null, revisado_por int, FOREIGN KEY (revisado_por) REFERENCES Autor(id));
