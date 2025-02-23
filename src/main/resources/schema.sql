c14e70d271803267e2aa26ca2f7f8b1df51cd62e
kikem <kikem@DESKTOP-NFD6NUR>
2025-02-23 14:19:33 +0100

    a

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,12 +3,15 @@
 
 --Para giis.demo.tkrun:
 drop if exists table Autor;
+drop if exists table Autor_articulo;
+drop if exists table Articulo;
+
 create table Autor(id int primary key not null, nombre varchar not null, organizacion varchar not null, grupo varchar not null, revisor boolean not null);
 
-drop if exists table Autor_articulo;
+
 create table Autor_articulo(idAutor int not null, idArticulo int not null, PRIMARY KEY(idAutor,idArticulo),
 	FOREIGN KEY(idAutor) REFERENCES Autor(id), FOREIGN KEY(idArticulo) REFERENCES Articulo(id));
 
-drop if exists table Articulo;
+
 create table Articulo(id int primary key not null, titulo varchar not null, palabras_clave varchar not null, resumen varchar not null,
  fichero varchar not null, fecha date not null);
