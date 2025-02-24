4feafb4423c4289aef02927d57a09ecc8425531f
kikem <kikem@DESKTOP-NFD6NUR>
2025-02-24 19:16:15 +0100

    a

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,12 +7,13 @@ DROP TABLE IF EXISTS Autor_articulo;
 DROP TABLE IF EXISTS Articulo;
 DROP TABLE IF EXISTS Autor;
 
-create table Autor(id int primary key not null, nombre varchar not null, organizacion varchar not null, grupo varchar not null, revisor boolean not null);
+create table Autor(id int primary key not null, nombre varchar not null, organizacion varchar not null, grupo varchar not null, revisor boolean not null,
+	coordinador boolean not null);
 
 
-create table Autor_articulo(idAutor int not null, idArticulo int not null, PRIMARY KEY(idAutor,idArticulo),
+create table Autor_articulo(idAutor int not null, idArticulo int not null, envia boolean not null, revisa boolean not null, PRIMARY KEY(idAutor,idArticulo),
 	FOREIGN KEY(idAutor) REFERENCES Autor(id), FOREIGN KEY(idArticulo) REFERENCES Articulo(id));
 
 
 create table Articulo(id int primary key not null, titulo varchar not null, palabras_clave varchar not null, resumen varchar not null,
- fichero varchar not null, fecha date not null, revisado_por int, FOREIGN KEY (revisado_por) REFERENCES Autor(id));
+ 	fichero varchar not null, fecha date not null);
