de76a09eb458c5a724c6b8f451ec7ca07678cf5f
kikem <kikem@DESKTOP-NFD6NUR>
2025-03-04 14:44:33 +0100

    Arreglo bases de datos

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-    --Primero se deben borrar todas las tablas (de detalle a maestro) y lugo anyadirlas (de maestro a detalle)
-=======
---Primero se deben borrar todas las tablas (de detalle a maestro) y lugo anyadirlas (de maestro a detalle)
->>>>>>> branch 'main' of https://github.com/JorgeGarciaRodriguez/SI2024-PL22.git
---(en este caso en cada aplicacion se usa solo una tabla, por lo que no hace falta)
-=======
->>>>>>> branch 'main' of https://github.com/JorgeGarciaRodriguez/SI2024-PL22.git
-
-<<<<<<< HEAD
---Para giis.demo.tkrun:
-
-DROP TABLE IF EXISTS Autor_articulo;
-DROP TABLE IF EXISTS Revision;
-DROP TABLE IF EXISTS Articulo;
-DROP TABLE IF EXISTS Persona;
-DROP TABLE IF EXISTS Autor;
-DROP TABLE IF EXISTS Revisor;
-
-create table Persona(id int primary key not null, nombre varchar not null, organizacion varchar not null, grupo varchar not null);
-
-create table Revisor(idRevisor int primary key not null, FOREIGN KEY(idRevisor) REFERENCES Persona(id));
-
-create table Autor(idAutor int primary key not null, correo varchar unique not null, FOREIGN KEY(idAutor) REFERENCES Persona(id));
-
-create table Autor_articulo(idAutor int not null, idArticulo int not null, envia boolean not null, PRIMARY KEY(idAutor,idArticulo),
-	FOREIGN KEY(idAutor) REFERENCES Autor(id), FOREIGN KEY(idArticulo) REFERENCES Articulo(id));
-
-create table Articulo(id int primary key not null, titulo varchar not null, palabras_clave varchar not null, resumen varchar not null,
- 	fichero varchar not null, fecha date not null, decisionfinal boolean);
- 	
-=======
 --Primero se deben borrar todas las tablas (de detalle a maestro) y lugo anyadirlas (de maestro a detalle)
 --(en este caso en cada aplicacion se usa solo una tabla, por lo que no hace falta)
 
@@ -55,6 +22,5 @@ create table Autor_articulo(idAutor int not null, idArticulo int not null, envia
 create table Articulo(id int primary key not null, titulo varchar not null, palabras_clave varchar not null, resumen varchar not null,
  	fichero varchar not null, fecha date not null, decisionfinal boolean);
  	
->>>>>>> branch 'main' of https://github.com/JorgeGarciaRodriguez/SI2024-PL22.git
 create table Revision(idRevision int not null, idRevisor int not null, idArticulo int not null,experto int,decision int,coment_autor varchar,
 	coment_coor varchar, PRIMARY KEY(idRevisor,idArticulo));
